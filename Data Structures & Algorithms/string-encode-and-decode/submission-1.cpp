af0e13bee7e1f35f3c69ce406239b2c8c1d2e5b8
Amisha Singla <78135215+Amisha121102@users.noreply.github.com>
2026-09-17 12:16:47 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+      string s = "";
+      for(auto word:strs){
+        s = s + to_string(word.size()) + '#' + word;
+      }
+      return s;
+}
+
+vector<string> decode(string s) {
+    vector<string> ans;
+    int i = 0;
+    while(i < (int)s.size()){
+        int j = i;
+        while(s[j] != '#') j++;
+        int len = stoi(s.substr(i,j-i));
+        ans.push_back(s.substr(j+1, len));
+        i = j+1+len;
+    }
+    return ans;
+}
+
+};
